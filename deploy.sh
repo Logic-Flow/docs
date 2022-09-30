a5a4071ef5d196e933c9aee97dec0b6cdb6c2c59
wumail <1059037014@qq.com>
2022-09-30 14:30:38 +0800

    feat: have a try

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,47 @@
 # 发生错误时终止
 set -e
 
-git add -A
-git commit -m "auto push while running deploy.sh"
-git push
+if branch=$(git symbolic-ref --short -q HEAD)
+then
+    cd examples
+    npm run build
+    cd -
+    git add -A
+    git commit -m "auto push while running deploy.sh"
+    git push
+    git checkout master
+    git merge $branch
+    git push
+    git push -f git@github.com:logic-flow/docs.logic-flow.cn.git master:gh-pages
+else
+    echo not on any branch
+fi
 
-cd examples
+# git add -A
+# git commit -m "auto push while running deploy.sh"
+# git push
 
-git checkout master
-# 构建
-npm run build
+# cd examples
 
-# 进入构建文件夹
-cd dist
+# # 构建
+# npm run build
 
-# 如果你要部署到自定义域名
-# echo 'www.example.com' > CNAME
+# # 进入构建文件夹
+# cd dist
 
-git add -A
-git commit -m 'deploy'
+# # 如果你要部署到自定义域名
+# # echo 'www.example.com' > CNAME
 
-# 如果你要部署在 https://<USERNAME>.github.io
+# git checkout master
+# git add -A
+# git commit -m 'deploy'
 
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# # 如果你要部署在 https://<USERNAME>.github.io
 
-# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:logic-flow/docs.logic-flow.cn.git master:gh-pages
+# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-cd -
+# # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+# # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# git push -f git@github.com:logic-flow/docs.logic-flow.cn.git master:gh-pages
+
+# cd -
