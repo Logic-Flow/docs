44325ed73ac1bdbaab7270f5dfea1e04840c9eaf
wumail <1059037014@qq.com>
2022-09-30 13:43:47 +0800

    auto push while running deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你要部署在 https://<USERNAME>.github.io
+
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
