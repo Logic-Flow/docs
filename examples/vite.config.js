404193cefbd6989402214b6d41723fa0b504f7c7
jerryjiang <jerryjiang@didiglobal.com>
2022-10-25 12:00:22 +0800

    fix: 修改示例路径

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -7,7 +7,7 @@ import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/docs.logic-flow.cn/examples/",
+  base: "/docs/examples/",
 
   plugins: [
     vue({
