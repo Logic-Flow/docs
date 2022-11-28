67d574f8b7104f483edcc557194571307c984b97
haozhangzhang <haozhangzhang@didiglobal.com>
2022-11-28 16:19:23 +0800

    feat:组织架构图开发完成

---
+++ examples/examples/案例/business-scenario/organization/htmlCard.js
@@ -0,0 +1,71 @@
+class HtmlCard extends HtmlNode {
+  // 重写HtmlNode的setHtml，来控制html节点内容。
+  setHtml(rootEl) {
+    const { properties } = this.props.model;
+    const html= `
+      ${
+        properties.gender === 'male' ? 
+        '<img src="https://img-hxy021.didistatic.com/static/starimg/img/7GdwliYTKp1669532392610.png" alt=""  width="60px" height="60px" />' :
+        '<img src="https://img-hxy021.didistatic.com/static/starimg/img/6tKEIAgA2k1669532400487.png" alt=""  width="60px" height="60px" />'
+      }
+      <div class="staff-detail">
+        <div class="staff-name">${properties.name}</div>
+        <div class="staff-position">${properties.position}</div>
+        <div class="staff-department">${properties.department}</div>
+      </div>
+      <div class="staff-options">
+        <div class="node-delete" onclick="updateNode('${properties.id}', 'delete')">
+          <img src="https://img-hxy021.didistatic.com/static/starimg/img/DyFI0DgXJb1669537739785.png" alt=""  width="20px" height="20px" />
+        </div>
+        <div class="node-add" onclick="updateNode('${properties.id}', 'add')">
+          <img src="https://img-hxy021.didistatic.com/static/starimg/img/4YJ5oVGRhF1669467073849.png" alt=""  width="20px" height="20px" />
+        </div>
+      </div>
+    `;
+    const el = document.createElement('div');
+    el.className = 'mind-mapping-node';
+    el.innerHTML = html;
+    rootEl.innerHTML = '';
+    rootEl.appendChild(el);
+    window.updateNode = (id, type) => {
+      const { graphModel } = this.props;
+      graphModel.eventCenter.emit("custom:node-update", {
+        id,
+        type,
+      });
+    };
+  }
+}
+
+class HtmlCardModel extends HtmlNodeModel {
+  initNodeData(data) {
+    super.initNodeData(data);
+    this.width = 250;
+    this.height = 80;
+  }
+
+  getDefaultAnchor() {
+    const { width, height, x, y, id } = this;
+    return [
+      {
+        x,
+        y: y - height / 2 + 11,
+        name: 'top',
+        id: `${id}_0`
+      },
+      {
+        x,
+        y: y + height / 2 - 11,
+        name: 'bottom',
+        id: `${id}_1`,
+        edgeAddable: false
+      },
+    ]
+  }
+}
+
+export default {
+  type: 'organization-node',
+  view: HtmlCard,
+  model: HtmlCardModel,
+}
---
+++ examples/examples/案例/business-scenario/organization/index.js
@@ -0,0 +1,246 @@
+import organizationNode from "./htmlCard.js";
+import organizationEdge from './htmlEdge.js';
+
+// logicflow流程图配置
+export const LFConfig = {
+  edgeTextDraggable: true,
+  adjustEdgeMiddle: true,
+  hoverOutline: false,
+  nodeSelectedOutline: false,
+  hideAnchors: true,
+  nodeTextEdit: false,
+  background: {
+    backgroundImage:
+            "url('https://dpubstatic.udache.com/static/dpubimg/805efa14-d908-4332-a7a2-bee8fbb903e3.jpg')",
+    backgroundRepeat: 'repeat',
+  },
+  grid: {
+    size: 1,
+    visible: false,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  style: {
+    edgeText: {
+      background: {
+        fill: '#F7F7F7',
+      },
+      hoverBackground: {
+        fill: '#F7F7F7',
+      },
+    },
+  },
+};
+const container = document.querySelector('#container');
+
+const lf = new LogicFlow({
+  ...LFConfig,
+  container,
+});
+
+lf.register(organizationNode);
+lf.register(organizationEdge);
+
+lf.setTheme({ 
+  arrow: {
+    offset: 0, // 箭头垂线长度
+    verticalLength: 0, // 箭头底线长度
+  }
+});
+
+let enterNode = null;
+
+lf.on('node:mouseenter', ({e}) => {
+  enterNode = e.target;
+  for (let index = 0; index < e.target.children.length; index++) {
+    const element = e.target.children[index];
+    if (element.className === 'staff-options') {
+      element.style.visibility = 'visible';
+    }
+  }
+});
+
+lf.on('node:mouseleave', (e) => {
+  for (let index = 0; index < enterNode.children.length; index++) {
+    const element = enterNode.children[index];
+    if (element.className === 'staff-options') {
+      element.style.visibility = 'hidden';
+    }
+  }
+});
+
+const baseData =  {
+  id: '1',
+  x: 100,
+  y: 100,
+  type: 'html-card',
+  gender: 'male',
+  name: 'Employee 1',
+  position: 'position',
+  department: 'department',
+  children: [
+    {
+      id: '2',
+      type: 'html-card',
+      gender: 'female',
+      name: 'Employee 2',
+      position: 'position',
+      department: 'department',
+      children: [
+        {
+          id: '4',
+          type: 'html-card',
+          gender: 'male',
+          name: 'Employee 4',
+          position: 'position',
+          department: 'department',
+        },
+        {
+          id: '5',
+          type: 'html-card',
+          gender: 'female',
+          name: 'Employee 5',
+          position: 'position',
+          department: 'department',
+        }
+      ]
+    },
+    {
+      id: '3',
+      type: 'html-card',
+      gender: 'male',
+      name: 'Employee 3',
+      position: 'position',
+      department: 'department',
+      children: [
+        {
+          id: '6',
+          type: 'html-card',
+          gender: 'male',
+          name: 'Employee 6',
+          position: 'position',
+          department: 'department',
+        },
+      ],
+    }
+  ]
+};
+
+lf.on('custom:node-update', ({id, type}) => {
+  if (type === 'add') {
+    addNode(baseData, id);
+  } else {
+    deleteNode(baseData, id);
+  }
+  renderData(baseData);
+});
+
+const addNode = (baseData, id) => {
+  if (baseData.id === id) {
+    baseData.children = baseData.children ? [
+      ...baseData.children,
+      {
+        id: `${Math.round(Math.random() * 10000)}`,
+        type: 'html-card',
+        gender: Math.round(Math.random() * 10) % 2 === 0 ? 'male' : 'female',
+        name: 'New Employee',
+        position: 'position',
+        department: 'department',
+      }
+    ] : [
+      {
+        id: `${lf.graphModel.nodes.length + 1}`,
+        type: 'html-card',
+        gender: Math.round(Math.random() * 10) % 2 === 0 ? 'male' : 'female',
+        name: 'New Employee',
+        position: 'position',
+        department: 'department'
+      }
+    ];
+    return;
+  }
+  if (baseData.children && baseData.children.length) {
+    baseData.children.forEach((node) => {
+      addNode(node, id);
+    });
+  }
+};
+
+const deleteNode = (baseData, id, parent, nodeIndex) => {
+  if (baseData.id === id) {
+    if (!parent) {
+      alert('根节点不能删除');
+      return;
+    }
+    parent.children.splice(nodeIndex, 1);
+    return;
+  }
+  if (baseData.children && baseData.children.length) {
+    baseData.children.forEach((node, index) => {
+      deleteNode(node, id, baseData, index);
+    });
+  }
+};
+
+// 节点平移
+const moveX = 600;
+const moveY = 200;
+
+const renderData = (baseData) => {
+  const rootNode = window.Hierarchy.compactBox(baseData, {
+    direction: 'TB',
+    getId(d) {
+      return d.id;
+    },
+    getHGap() {
+      return 120;
+    },
+    getVGap() {
+      return 40;
+    },
+  });
+
+  const tansferNodes = (node, parent) => {
+    const nodes = [];
+    const edges = [];
+    const curNode = {
+      id: `node_${node.data.id}`,
+      x: node.x + moveX,
+      y: node.y + moveY,
+      type: 'organization-node',
+      properties: {
+        ...node.data,
+      },
+    };
+    nodes.push(curNode);
+    if (parent) {
+      edges.push({
+        id: `edge_${node.data.id}_${parent.properties.id}`,
+        sourceNodeId: parent.id,
+        targetNodeId: curNode.id,
+        type: 'organization-edge',
+      });
+    }
+    if (node.children && node.children.length) {
+      node.children.forEach((childNode) => {
+        const graphData = tansferNodes(childNode, curNode);
+        nodes.push(...graphData.nodes);
+        edges.push(...graphData.edges);
+      });
+    }
+    return {
+      nodes,
+      edges,
+    };
+  };
+
+  const graphData = tansferNodes(rootNode);
+  console.log(graphData);
+  lf.render(graphData);
+}
+
+renderData(baseData);
+// lf.focusOn({
+//   id: lf.graphModel.nodes[0].id,
+// });
