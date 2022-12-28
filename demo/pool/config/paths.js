a79ec890cdbbb4f7de7ea2eb621d8845326688cc
miaomiao12345 <candicechenhong@didichuxing.com>
2022-12-28 16:31:36 +0800

    fix:path修复

--- demo/pool/config/paths.js
+++ demo/pool/config/paths.js
@@ -69,7 +69,7 @@ module.exports = {
   appWebpackCache: resolveApp('node_modules/.cache'),
   appTsBuildInfoFile: resolveApp('node_modules/.cache/tsconfig.tsbuildinfo'),
   swSrc: resolveModule(resolveApp, 'src/service-worker'),
-  publicUrlOrPath: './',
+  publicUrlOrPath: process.env.NODE_ENV === 'production' ? '/demo/dist/pool/' : '/',
 };
 
 
