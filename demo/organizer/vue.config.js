06adc361dd03e935b32a6267da9bd9804ce2150f
jerryjiang <jerryjiang@didiglobal.com>
2023-06-15 11:09:27 +0800

    fix: publicPath of organizer

--- demo/organizer/vue.config.js
+++ demo/organizer/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/demo/dist/organizer/' : '/',
   transpileDependencies: true
 })
